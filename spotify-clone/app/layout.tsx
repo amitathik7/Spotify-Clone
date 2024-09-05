09cb681813e85b1c0fb809433b66333b2ced1859
amitathik7 <amitathik7@gmail.com>
2024-09-05 09:39:13 -0400

    Setup layout for spotify webpage.
    Home button
    Search button
    Library sidebar
    Forward/Backward navigation buttons
    Sign up and Log in buttons
    Liked Songs button
    etc...

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -2,21 +2,25 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 
+import Sidebar from "@/components/Sidebar";
+
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Spotify Clone",
-  description: "Listen to music",
+	title: "Spotify Clone",
+	description: "Listen to music",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={font.className}>
+				<Sidebar>{children}</Sidebar>
+			</body>
+		</html>
+	);
 }
